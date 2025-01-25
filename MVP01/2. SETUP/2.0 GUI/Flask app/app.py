4a4a8aefe619642518aee3a983cdbd9e32f48053
Alcedo Coenen <alcedo.coenen@gmail.com>
2025-01-25 20:57:14 +0100

    GUI with Flask setup added

--- MVP01/2. SETUP/2.0 GUI/Flask app/app.py
+++ MVP01/2. SETUP/2.0 GUI/Flask app/app.py
@@ -2,11 +2,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import os
+from config import *
 
 app = Flask(__name__)
 
 # Database setup (same as before)
 DATABASE = '/Users/alcedocoenen/Documents/Plus-Minus/Python/MetaPlus/MetaPlus/MVP01/2. SETUP/2.0 GUI/Config_database/configDB'
+
+realisation_db = RealisationDB(DATABASE)
+realisation_db.create_table()
+config_db = Config(DATABASE)
+config_db.create_table()
+
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
@@ -65,11 +72,169 @@ def save_realisation():
         conn.execute('INSERT INTO main.Realisation (name, author, version, number_of_layers) VALUES (?, ?, ?, ?)',
                      (name, author, version, number_of_layers))
     else:
-        conn.execute('UPDATE main.Realisation SET name = ?, author = ?, version = ?, number_of_layers = ? WHERE number = ?',
+        conn.execute('UPDATE main.Realisation SET name = ?, author = ?, version = ?, number_of_layers = ? WHERE id = ?',
                      (name, author, version, number_of_layers, id))
     conn.commit()
     conn.close()
     return redirect(url_for('realisations'))
+
+
+@app.route('/configuration/<int:realisation_number>', methods=['GET', 'POST'])
+def configuration(realisation_number):
+    realisation = realisation_db.get_by_id(realisation_number)
+    if realisation is None:
+        return "Realisation not found", 404
+    configurations = config_db.get_by_realisation_number(realisation_number)
+    selected_config = None
+    if request.args.get('layer_id'):
+        try:
+            selected_layer_id = int(request.args.get('layer_id'))
+            selected_config = config_db.get_by_id(selected_layer_id)
+        except ValueError:
+            pass
+
+    if request.method == 'POST':
+        if 'save_config' in request.form:
+            layer_id = int(request.form['layer_id'])
+            data = {
+                "name": request.form['name'],
+                "squarepages": request.form['squarepages'],
+                "notepages": request.form['notepages'],
+                "sequence_offset_start": int(request.form['sequence_offset_start']),
+                "sequence_offset_mid": int(request.form['sequence_offset_mid']),
+                "sequence_offset_end": int(request.form['sequence_offset_end']),
+                "staccato_duration": int(request.form['staccato_duration']),
+                "gracenote_offset": int(request.form['gracenote_offset']),
+                "cs_instrument": request.form['cs_instrument'],
+                "cs_midi_channel": int(request.form['cs_midi_channel']),
+                "cs_midi_instrument": int(request.form['cs_midi_instrument']),
+                "cs_def_volume": int(request.form['cs_def_volume']),
+                "cs_def_duration": int(request.form['cs_def_duration']),
+                "acc_instrument": request.form['acc_instrument'],
+                "acc_midi_channel": int(request.form['acc_midi_channel']),
+                "acc_midi_instrument": int(request.form['acc_midi_instrument']),
+                "acc_def_volume": int(request.form['acc_def_volume']),
+                "acc_def_duration": int(request.form['acc_def_duration']),
+                "acc_pitch_short": int(request.form['acc_pitch_short']),
+                "acc_pitch_medium": int(request.form['acc_pitch_medium']),
+                "acc_pitch_long": int(request.form['acc_pitch_long']),
+                "subs_instrument": request.form['subs_instrument'],
+                "subs_midi_channel": int(request.form['subs_midi_channel']),
+                "subs_midi_instrument": int(request.form['subs_midi_instrument']),
+                "subs_def_volume": int(request.form['subs_def_volume']),
+                "subs_def_duration": int(request.form['subs_def_duration']),
+                "subs_timepoint_distance": int(request.form['subs_timepoint_distance'])
+            }
+            if layer_id == 0:
+                data['realisation_number'] = realisation_number
+                config_db.insert(data)
+            else:
+                config_db.update(layer_id, data)
+            return redirect(url_for('configuration', realisation_number=realisation_number))
+        elif 'new_config' in request.form:
+            selected_config = {'layer_id': 0, 'name': '', 'squarepages': '', 'notepages': '',
+                               'sequence_offset_start': 0, 'sequence_offset_mid': 0, 'sequence_offset_end': 0,
+                               'staccato_duration': 0, 'gracenote_offset': 0, 'cs_instrument': '', 'cs_midi_channel': 0,
+                               'cs_midi_instrument': 0, 'cs_def_volume': 0, 'cs_def_duration': 0, 'acc_instrument': '',
+                               'acc_midi_channel': 0, 'acc_midi_instrument': 0, 'acc_def_volume': 0,
+                               'acc_def_duration': 0, 'acc_pitch_short': 0, 'acc_pitch_medium': 0, 'acc_pitch_long': 0,
+                               'subs_instrument': '', 'subs_midi_channel': 0, 'subs_midi_instrument': 0,
+                               'subs_def_volume': 0, 'subs_def_duration': 0, 'subs_timepoint_distance': 0}
+
+    return render_template('configuration.html', realisation=realisation, configurations=configurations,
+                           selected_config=selected_config)
+
+
+'''
+@app.route('/configuration/<int:realisation_number>', methods=['GET', 'POST'])
+def configuration(realisation_number):
+    conn = get_db_connection()
+    realisation = conn.execute('SELECT * FROM main.Realisation WHERE id = ?', (realisation_number,)).fetchone()
+    if realisation is None:
+        return "Realisation not found", 404
+    configurations = conn.execute('SELECT * FROM main.Config_Layer WHERE ref_to_realisation = ?', (realisation_number,)).fetchall()
+
+    selected_configuration = None
+    if request.args.get('layer_id'):
+        try:
+            selected_number = int(request.args.get('layer_id'))
+            selected_configuration = conn.execute('SELECT * FROM main.Config_Layer WHERE layer_id = ?', (selected_number,)).fetchone()
+        except ValueError:
+            pass # Handle invalid input
+    conn.close()
+
+    if request.method == 'POST':
+        layer_id = request.form['layer_id']
+        data = {
+            "name": request.form['name'],
+            "squarepages": request.form['squarepages'],
+            "notepages": request.form['notepages'],
+            "sequence_offset_start": int(request.form['sequence_offset_start']),
+            "sequence_offset_mid": int(request.form['sequence_offset_mid']),
+            "sequence_offset_end": int(request.form['sequence_offset_end']),
+            "staccato_duration": int(request.form['staccato_duration']),
+            "gracenote_offset": int(request.form['gracenote_offset']),
+            "cs_instrument": request.form['cs_instrument'],
+            "cs_midi_channel": int(request.form['cs_midi_channel']),
+            "cs_midi_instrument": int(request.form['cs_midi_instrument']),
+            "cs_def_volume": int(request.form['cs_def_volume']),
+            "cs_def_duration": int(request.form['cs_def_duration']),
+            "acc_instrument": request.form['acc_instrument'],
+            "acc_midi_channel": int(request.form['acc_midi_channel']),
+            "acc_midi_instrument": int(request.form['acc_midi_instrument']),
+            "acc_def_volume": int(request.form['acc_def_volume']),
+            "acc_def_duration": int(request.form['acc_def_duration']),
+            "acc_pitch_short": int(request.form['acc_pitch_short']),
+            "acc_pitch_medium": int(request.form['acc_pitch_medium']),
+            "acc_pitch_long": int(request.form['acc_pitch_long']),
+            "subs_instrument": request.form['subs_instrument'],
+            "subs_midi_channel": int(request.form['subs_midi_channel']),
+            "subs_midi_instrument": int(request.form['subs_midi_instrument']),
+            "subs_def_volume": int(request.form['subs_def_volume']),
+            "subs_def_duration": int(request.form['subs_def_duration']),
+            "subs_timepoint_distance": int(request.form['subs_timepoint_distance'])
+        }
+        if layer_id == 0:
+            data['realisation_number'] = realisation_number
+            config_db.insert(data)
+        else:
+            config_db.update(layer_id, data)
+        return redirect(url_for('configuration', realisation_number=realisation_number))
+
+        elif 'new_config' in request.form:
+        selected_config = {'layer_id': 0, 'name': '', 'squarepages': '', 'notepages': '', 'sequence_offset_start': 0,
+                           'sequence_offset_mid': 0, 'sequence_offset_end': 0, 'staccato_duration': 0, 'gracenote_offset': 0,
+                           'cs_instrument': '', 'cs_midi_channel': 0, 'cs_midi_instrument': 0, 'cs_def_volume': 0,
+                           'cs_def_duration': 0, 'acc_instrument': '', 'acc_midi_channel': 0, 'acc_midi_instrument': 0,
+                           'acc_def_volume': 0, 'acc_def_duration': 0, 'acc_pitch_short': 0, 'acc_pitch_medium': 0,
+                           'acc_pitch_long': 0, 'subs_instrument': '', 'subs_midi_channel': 0, 'subs_midi_instrument': 0,
+                           'subs_def_volume': 0, 'subs_def_duration': 0, 'subs_timepoint_distance': 0}
+
+        return render_template('configuration.html', realisation=realisation, configurations=configurations,
+                               selected_config=selected_config)
+
+columns = ['ref_to_realisation', 'layer_id', 'name', 'squarepages', 'notepages', 'sequence_offset_start',
+                   'sequence_offset_mid', 'sequence_offset_end', 'staccato_duration', 'gracenote_offset',
+                   'cs_instrument', 'cs_midi_channel', 'cs_midi_instrument', 'cs_def_volume', 'cs_def_duration',
+                   'acc_instrument', 'acc_midi_channel', 'acc_midi_instrument', 'acc_def_volume', 'acc_def_duration',
+                   'acc_pitch_short', 'acc_pitch_medium', 'acc_pitch_long', 'subs_instrument', 'subs_midi_channel',
+                   'subs_midi_instrument', 'subs_def_volume', 'subs_def_duration', 'subs_timepoint_distance']
+        columns = ', '.join(columns)
+        placeholders = ', '.join(['?'] * 29)
+        params = (realisation_number, layer_id, name, squarepages, notepages, sequence_offset_start,
+                  sequence_offset_mid, sequence_offset_end, staccato_duration, gracenote_offset,
+                  cs_instrument, cs_midi_channel, cs_midi_instrument, cs_def_volume, cs_def_duration,
+                  acc_instrument, acc_midi_channel, acc_midi_instrument, acc_def_volume, acc_def_duration,
+                  acc_pitch_short, acc_pitch_medium, acc_pitch_long, subs_instrument, subs_midi_channel,
+                  subs_midi_instrument, subs_def_volume, subs_def_duration, subs_timepoint_distance)
+        sql = f"INSERT INTO main.Config_Layer ({columns}) VALUES ({placeholders})"
+        conn.execute(sql, params)
+        conn.commit()
+
+        return redirect(url_for('configuration', realisation_number=realisation_number))
+    conn.close()
+    return render_template('configuration.html', realisation=realisation, configurations=configurations)
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ MVP01/2. SETUP/2.0 GUI/Flask app/config.py
@@ -0,0 +1,232 @@
+import sqlite3
+
+
+class Config:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = None
+
+    def _connect(self):
+        try:
+            self.conn = sqlite3.connect(self.db_path)
+            self.conn.row_factory = sqlite3.Row  # Return rows as dictionaries
+            return self.conn
+        except sqlite3.Error as e:
+            print(f"Database connection error: {e}")
+            return None
+
+    def _close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+    def create_table(self):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS config (
+                        layer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        squarepages TEXT,
+                        notepages TEXT,
+                        sequence_offset_start INTEGER,
+                        sequence_offset_mid INTEGER,
+                        sequence_offset_end INTEGER,
+                        staccato_duration INTEGER,
+                        gracenote_offset INTEGER,
+                        cs_instrument TEXT,
+                        cs_midi_channel INTEGER,
+                        cs_midi_instrument INTEGER,
+                        cs_def_volume INTEGER,
+                        cs_def_duration INTEGER,
+                        acc_instrument TEXT,
+                        acc_midi_channel INTEGER,
+                        acc_midi_instrument INTEGER,
+                        acc_def_volume INTEGER,
+                        acc_def_duration INTEGER,
+                        acc_pitch_short INTEGER,
+                        acc_pitch_medium INTEGER,
+                        acc_pitch_long INTEGER,
+                        subs_instrument TEXT,
+                        subs_midi_channel INTEGER,
+                        subs_midi_instrument INTEGER,
+                        subs_def_volume INTEGER,
+                        subs_def_duration INTEGER,
+                        subs_timepoint_distance INTEGER
+                    )
+                """)
+                conn.commit()
+                print("Table 'config' created (or already exists).")
+            except sqlite3.Error as e:
+                print(f"Error creating table: {e}")
+            finally:
+                self._close()
+
+    def insert(self, data):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                placeholders = ", ".join(["?"] * len(data))
+                columns = ", ".join(data.keys())
+                sql = f"INSERT INTO config ({columns}) VALUES ({placeholders})"
+                cursor.execute(sql, list(data.values()))
+                conn.commit()
+                return cursor.lastrowid  # return the ID of the last inserted row
+            except sqlite3.Error as e:
+                print(f"Error inserting data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def update(self, layer_id, data):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                updates = ", ".join([f"{key} = ?" for key in data.keys()])
+                sql = f"UPDATE config SET {updates} WHERE layer_id = ?"
+                values = list(data.values())
+                values.append(layer_id)
+                cursor.execute(sql, values)
+                conn.commit()
+                return cursor.rowcount  # return the number of updated rows
+            except sqlite3.Error as e:
+                print(f"Error updating data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def get_by_id(self, layer_id):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM config WHERE layer_id = ?", (layer_id,))
+                return cursor.fetchone()
+            except sqlite3.Error as e:
+                print(f"Error getting data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def get_by_realisation_number(self, realisation_number):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                conn.execute('SELECT * FROM main.Config_Layer WHERE ref_to_realisation = ?',
+                             (realisation_number,)).fetchall()
+                #cursor.execute("SELECT * FROM config WHERE realisation_number = ?", (realisation_number,))
+                return cursor.fetchone()
+            except sqlite3.Error as e:
+                print(f"Error getting data: {e}")
+                return None
+            finally:
+                self._close()
+
+
+
+class RealisationDB:  # class for realisation table
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = None
+        self.table_name = "realisations"
+
+    def _connect(self):  # same as in ConfigDB
+        try:
+            self.conn = sqlite3.connect(self.db_path)
+            self.conn.row_factory = sqlite3.Row
+            return self.conn
+        except sqlite3.Error as e:
+            print(f"Database connection error: {e}")
+            return None
+
+    def _close(self):  # same as in ConfigDB
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+    def create_table(self):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"""
+                    CREATE TABLE IF NOT EXISTS {self.table_name} (
+                        number INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL,
+                        author TEXT,
+                        version TEXT,
+                        number_of_layers INTEGER
+                    )
+                """)
+                conn.commit()
+                print(f"Table '{self.table_name}' created (or already exists).")
+            except sqlite3.Error as e:
+                print(f"Error creating table: {e}")
+            finally:
+                self._close()
+
+    def get_all(self):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT * FROM {self.table_name}")
+                return cursor.fetchall()
+            except sqlite3.Error as e:
+                print(f"Error getting data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def get_by_id(self, number):
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT * FROM {self.table_name} WHERE number = ?", (number,))
+                return cursor.fetchone()
+            except sqlite3.Error as e:
+                print(f"Error getting data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def insert(self, data):  # similar to ConfigDB
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                placeholders = ", ".join(["?"] * len(data))
+                columns = ", ".join(data.keys())
+                sql = f"INSERT INTO {self.table_name} ({columns}) VALUES ({placeholders})"
+                cursor.execute(sql, list(data.values()))
+                conn.commit()
+                return cursor.lastrowid
+            except sqlite3.Error as e:
+                print(f"Error inserting data: {e}")
+                return None
+            finally:
+                self._close()
+
+    def update(self, number, data):  # similar to ConfigDB
+        conn = self._connect()
+        if conn:
+            try:
+                cursor = conn.cursor()
+                updates = ", ".join([f"{key} = ?" for key in data.keys()])
+                sql = f"UPDATE {self.table_name} SET {updates} WHERE number = ?"
+                values = list(data.values())
+                values.append(number)
+                cursor.execute(sql, values)
+                conn.commit()
+                return cursor.rowcount
+            except sqlite3.Error as e:
+                print(f"Error updating data: {e}")
+                return None
+            finally:
+                self._close()
